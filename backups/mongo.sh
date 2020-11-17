46ac00cbe27dbdf3800f729c12a34cf77c3f5afb
Danail Nedyalkov <danail.nedyalkov@yahoo.com>
2020-11-18 00:36:40 +0200

    Refactoring mongodb backup script

--- backups/mongo.sh
+++ backups/mongo.sh
@@ -8,16 +8,23 @@ auth_db="admin"
 
 # Other options
 backup_path="/backups"
-date=$(date +"%Y-%b-%d")
+date=$(date +"%Y-%m-%d")
 
 # Set default file permissions
 umask 177
 
-# Dump database into archived file
-mongodump --db ufeelme --host=$host --username $user --password $password --authenticationDatabase $auth_db --archive=$backup_path/$db_name-$date.gz --gzip
+# Dump database into folder
+mongodump --quiet --db ufeelme --host=$host --username $user --password $password --authenticationDatabase $auth_db -o $backup_path/$db_name-$date
+
+# archive dump folder
+cd $backup_path
+tar czf $db_name-$date.tar.gz $db_name-$date
+
+# remove dump folder
+rm -rf $db_name-$date
 
 # Change permissions
-chmod 0644 $backup_path/$db_name-$date.gz
+chmod 0644 $db_name-$date.tar.gz
 
 # Delete files older than 30 days
 find $backup_path/* -mtime +30 -exec rm {} \;
